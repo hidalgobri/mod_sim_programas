4b0af5598242e82915fba9f9fbb1a0ecd83328c2
valeria <valeriahidalgo50@gmail.com>
2021-11-25 10:14:47 -0500

    imprime los primeros hijos del nodo padre

--- misioneros_canibales/main.cpp
+++ misioneros_canibales/main.cpp
@@ -5,15 +5,18 @@ const int FILAS_MATRIZ_POSIB = 5;
 const int COLUMNAS_MATRIZ_POSIB = 3;
 
 //matrices con todas las opciones posibles
+
 //cuando el barco es 0 --> esta en b
-//como queda a, cuando el barco se
+
+//como queda a, cuando el barco viaja de A a B
 
 int posibilidades_ab[5][3] = {{0,-1,-1} //misionero, canibal, barco
                         , {0,-2,-1}
                         , {-1,0,-1}
                         , {-2,0,-1}
                         , {-1,-1,-1}};
-//como queda a
+
+//como queda a, cuando el barco viaja de B a A
 int posibilidades_ba[5][3] = {{0,1,1}
                             ,{0,2,1}
                             ,{1,0,1}
@@ -39,6 +42,39 @@ Node *newNode(int misioneros, int canibales, int barco)
     return temp;
 }
 
+// Prints the n-ary tree level wise
+void levelOrderTraversal(Node * root)
+{
+    if (root==NULL)
+        return;
+
+    // Standard level order traversal code
+    // using queue
+    std::queue<Node *> q;  // Create a queue
+    q.push(root); // Enqueue root
+    while (!q.empty())
+    {
+        int n = q.size();
+
+        // If this node has children
+        while (n > 0)
+        {
+            // Dequeue an item from queue and print it
+            Node * p = q.front();
+            q.pop();
+            std::cout << "M"<<p->misioneros << " C"<<p->canibales<<" B"<<p->barco<<"  ||  " ;
+
+            // Enqueue all children of the dequeued item
+            for (int i=0; i<p->child.size(); i++)
+                q.push(p->child[i]);
+            n--;
+        }
+
+        std::cout << std::endl; // Print new line between two levels
+    }
+}
+
+//función condicion
 int *opciones_nodos_hijos(Node *padre) //verifica que opciones de todas las posibles, cumplen con la condición
 {
 
@@ -94,18 +130,29 @@ int *opciones_nodos_hijos(Node *padre) //verifica que opciones de todas las posi
 
 int main()
 {
-
     //Creo el nodo padre con el estado inicial M3C3B1
     Node *root = newNode(3 ,3 ,1);
 
     int *opciones_hijos = opciones_nodos_hijos(root);
 
-    std::cout<<"hello vale, las respuestas: "<<opciones_hijos[0]<<std::endl;
-
-    for(int y=0;y<5;y++)  // loop for the three elements on the line
+    if(root->barco == 1)//barco en a
     {
-        std::cout<<"hello vale, las respuestas: "<<opciones_hijos[y]<<std::endl;
+
+        for (int i = 0 ; i < 5 ; i++)
+        {
+            if(opciones_hijos[i] != -1)
+            {
+                int actual_misioneros = root->misioneros+posibilidades_ab[i][0];
+                int actual_canibales = root->canibales+posibilidades_ab[i][1];
+                int actual_barco = root->barco + posibilidades_ab[i][2];
+
+                (root->child).push_back(newNode(actual_misioneros,actual_canibales,actual_barco));
+            }
+        }
     }
+    std::cout<< "hello brigitte, la respuesta: \n";
+    levelOrderTraversal(root);
+
 
     return 0;
 }
